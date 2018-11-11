35daa8db38a694f15d21219229e2976dfc763604
Crazyerick <1186665051@qq.com>
2018-11-12 01:07:17 +0800

    添加显示天气信息的功能

--- app/src/main/java/com/erick/hotweather/ChooseAreaFragment.java
+++ app/src/main/java/com/erick/hotweather/ChooseAreaFragment.java
@@ -108,22 +108,20 @@ public class ChooseAreaFragment extends Fragment {
                     selectedCity = mCityList.get(position);
                     queryCounties();
                 } else if (currentLevel == LEVEL_COUNTY) {
-//                    String weatherId = mCountyList.get(position).getWeatherId();
-//                    if (getActivity() instanceof MainActivity) {
-//                        Intent intent = new Intent(getActivity(), WeatherActivity.class);
-//                        intent.putExtra("weather_id", weatherId);
-//                        startActivity(intent);
-//                        getActivity().finish();
-//                    } else if (getActivity() instanceof WeatherActivity) {
+                    String weatherId = mCountyList.get(position).getWeatherId();
+                    if (getActivity() instanceof MainActivity) {
+                        Intent intent = new Intent(getActivity(), WeatherActivity.class);
+                        intent.putExtra("weather_id", weatherId);
+                        startActivity(intent);
+                        getActivity().finish();
+                    }
+//                    else if (getActivity() instanceof WeatherActivity) {
 //                        WeatherActivity activity = (WeatherActivity) getActivity();
 //                        activity.drawerLayout.closeDrawers();
 //                        activity.swipeRefresh.setRefreshing(true);
 //                        activity.requestWeather(weatherId);
 //                    }
-
                 }
-
-
             }
         });
         backButton.setOnClickListener(new View.OnClickListener() {
---
+++ app/src/main/java/com/erick/hotweather/gson/AQI.java
@@ -0,0 +1,15 @@
+package com.erick.hotweather.gson;
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity {
+
+        public String aqi;
+
+        public String pm25;
+
+    }
+
+}
