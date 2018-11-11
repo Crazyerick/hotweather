5b017c400508a56a34d5bcc0ab274a466fad8390
erickxhyang <erickxhyang@tencent.com>
2018-11-11 15:46:13 +0800

    创建数据库和表的相关配置

---
+++ app/src/main/java/com/erick/hotweather/app/HotApplication.java
@@ -0,0 +1,30 @@
+package com.erick.hotweather.app;
+
+import android.app.Application;
+import android.content.Context;
+
+import org.litepal.LitePal;
+
+/**
+ * Created by erickxhyang on 2018/3/13.
+ */
+
+public class HotApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        LitePal.initialize(this);
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+    }
+
+}
